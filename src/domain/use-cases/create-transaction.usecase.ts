6b3923cc65f263dff79079389b2e831609784141
Jeferson Apaza <jefersonapaza@gmail.com>
2025-12-14 22:10:22 -0500

    feat(domain): add CreateTransactionUseCase with initial status logic

--- src/domain/use-cases/create-transaction.usecase.ts
+++ src/domain/use-cases/create-transaction.usecase.ts
@@ -1,19 +1,43 @@
 import { TransactionRepository } from '../repositories/transaction.repository';
 import { Transaction } from '../entities/transaction.entity';
 import { randomUUID } from 'crypto';
+import { KafkaPublisher } from '../../infrastructure/kafka/kafka-publisher';
+
+export interface CreateTransactionDTO {
+  accountExternalIdDebit: string;
+  accountExternalIdCredit: string;
+  tranferTypeId: number;
+  value: number;
+}
 
 export class CreateTransactionUseCase {
   constructor(
-    private readonly repository: TransactionRepository
+    private readonly repository: TransactionRepository,
+    private readonly kafkaPublisher: KafkaPublisher,
   ) {}
 
-  async execute(amount: number): Promise<void> {
+  async execute(dto: CreateTransactionDTO) {
+    const status = dto.value > 1000 ? 'rejected' : 'pending';
+
     const transaction = new Transaction(
       randomUUID(),
-      amount,
-      new Date()
+      dto.accountExternalIdDebit,
+      dto.accountExternalIdCredit,
+      dto.tranferTypeId,
+      dto.value,
+      status,
+      new Date(),
     );
 
     await this.repository.save(transaction);
+
+    // Emitir evento a Kafka
+    await this.kafkaPublisher.publish('transaction.created', {
+      id: transaction.id,
+      value: transaction.value,
+      status: transaction.status,
+    });
+
+    return transaction;
   }
 }
